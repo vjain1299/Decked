a8fb59ba771e2fb6c8d6a9b36d78c97b6c3ef861
Anirudh Lagisetty <aniusa2010@gmail.com>
2019-08-21 00:21:22 -0400

    Settings completed

--- Source/Decked/app/src/main/java/com/ani/decked/SettingsActivity.kt
+++ Source/Decked/app/src/main/java/com/ani/decked/SettingsActivity.kt
@@ -2,6 +2,8 @@ package com.ani.decked
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.EditTextPreference
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 
 //name, color of cards
@@ -24,9 +26,25 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     private class SettingsFragment : PreferenceFragmentCompat() {
+
+        var userName : String = "" /*TODO: This is just here for variable purposes, I do believe that this needs to be moved to the main actiivity*/
+        var color : String = "" // Same comment as above for this
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences_settings, rootKey)
+        }
 
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            val prefName = findPreference<EditTextPreference>("prefName")
+            val cardColor = findPreference<ListPreference>("cardColor") /*TODO: Does this mean that I would need to create a cardColor in MainActivity?*/
+
+            if(prefName!!.text == null) {
+                prefName.setDefaultValue("Player") /*TODO: What do we want our default value to be?*/
+            }
+            /*TODO: Do we need to create a new variable to save the player's name in the main activity?*/
+            else userName = prefName.text
+
+            color = cardColor!!.value /*TODO: I think we can set the default color to begin with and then just set it here to whatever value shows up from the settings*/
 
         }
     }
